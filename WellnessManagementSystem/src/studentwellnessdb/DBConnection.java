c8f2592a9174ea937c6e6dd52ebfe1b1f4e54e1c
Ulrigh <ulrighoos@gmail.com>
2025-07-16 16:59:08 +0200

    removed old DB + added new DB

--- WellnessManagementSystem/src/studentwellnessdb/DBConnection.java
+++ WellnessManagementSystem/src/studentwellnessdb/DBConnection.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
-    private static final String DB_URL = "jdbc:derby:E:\\Desktop\\College Things\\PRG_381\\PRG381_Project2\\StudentWellnessDB\\Database\\wellnessDB;create=true";
+    private static final String DB_URL = "jdbc:derby:E:\\Desktop\\College Things\\PRG_381\\PRG381_Project2\\WellnessManagementSystem\\Database\\wellnessDB;create=true";
     private static final String DRIVER = "org.apache.derby.jdbc.EmbeddedDriver";
 
     public static Connection getConnection() throws ClassNotFoundException, SQLException {
