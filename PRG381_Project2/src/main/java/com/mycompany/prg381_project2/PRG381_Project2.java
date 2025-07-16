8d12adefbf50e5c5b1ca403d317c9a0c985ffb08
NeoPolori <neonjabulo95@gmail.com>
2025-07-16 10:12:51 +0200

    Initialize Maven Netbeans project for

---
+++ PRG381_Project2/src/main/java/com/mycompany/prg381_project2/PRG381_Project2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.prg381_project2;
+
+/**
+ *
+ * @author neonj
+ */
+public class PRG381_Project2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
