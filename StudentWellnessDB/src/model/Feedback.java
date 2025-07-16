99553ac8f4547c868304be2156db09e37e23a455
Ulrigh <ulrighoos@gmail.com>
2025-07-16 15:03:37 +0200

    Removed old DB
    Replaced database creations string location and made new DB

--- StudentWellnessDB/src/model/Feedback.java
+++ StudentWellnessDB/src/model/Feedback.java
@@ -6,17 +6,50 @@ public class Feedback {
     private int rating;
     private String comments;
 
+    // Constructor without ID
     public Feedback(String student, int rating, String comments) {
         this.student = student;
         this.rating = rating;
         this.comments = comments;
     }
 
+    // Constructor with ID
     public Feedback(int id, String student, int rating, String comments) {
         this(student, rating, comments);
         this.id = id;
     }
 
-    // Getters & Setters
-}
+    // --- Getters ---
+    public int getId() {
+        return id;
+    }
 
+    public String getStudent() {
+        return student;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public String getComments() {
+        return comments;
+    }
+
+    // --- Setters ---
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setStudent(String student) {
+        this.student = student;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public void setComments(String comments) {
+        this.comments = comments;
+    }
+}
--- StudentWellnessDB/src/studentwellnessdb/StudentWellnessDB.java
+++ StudentWellnessDB/src/studentwellnessdb/StudentWellnessDB.java
@@ -7,6 +7,7 @@ public class StudentWellnessDB {
     public static void main(String[] args) {
         try (Connection conn = DBConnection.getConnection()) {
             System.out.println("Connected to JavaDB!");
+            DbInit.TableCreation();
         } catch (Exception e) {
             System.out.println("Error: " + e.getMessage());
         }
